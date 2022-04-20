53bb127a88d31d253ebf33792d3895f010109d1c
zizopixels <zizopixels11@gmail.com>
2022-04-20 13:06:16 +0500

    farooque branch chnage 1

--- project-name/urls.py
+++ project-name/urls.py
@@ -5,11 +5,9 @@ from bonds_app.views import *
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
-    TokenVerifyView,
-)
+    TokenVerifyView,)
 
-
-
+#farooque branch
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -19,7 +17,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-
-
 
 ]
