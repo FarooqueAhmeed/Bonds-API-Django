fc66a802efe751f579d419e74bee0f47f9cca959
zizopixels <zizopixels11@gmail.com>
2022-04-20 15:52:14 +0500

    farooque branch change #9

--- project-name/urls.py
+++ project-name/urls.py
@@ -7,7 +7,8 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView,)
 
-#change #10
+#farooque branch
+#change #9
 
 urlpatterns = [
     path('admin/', admin.site.urls),
